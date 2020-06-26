296359a34d96c53d18c4aef9eabdbfdb5008f767
KML122089 <65563104+KML122089@users.noreply.github.com>
2020-06-25 20:38:25 -0400

    inputs and URL format

--- flight.py
+++ flight.py
@@ -1,20 +1,34 @@
 import requests
-import os
-from dotenv import load_dotenv
+from bs4 import BeautifulSoup
 
-load_dotenv() 
 
-url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/"
+origin_code = input("TYPE YOUR AIRPORT CODE HERE:") #origin code
+start_date = input("TYPE YOUR DATE HERE (FORMAT:YYYY-MM-DD):") #flight date
+end_date = input("TYPE YOUR END DATE HERE (FORMAT:YYYY-MM-DD):")
 
-querystring = {"query":"Stockholm"}
+ # note that the import package command is `bs4`
 
-headers = {
-    'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-    'x-rapidapi-key': "SKYSCANNER_API_KEY"
-    }
+response = requests.get(f"https://skiplagged.com/flights/{origin_code}/{start_date}/{end_date}")
+response_html = response.text
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+# soup = BeautifulSoup(response_html)
 
-print(response.text)
+# titles = soup.find_all("span", "title")
 
-api_key = os.environ.get("SKYSCANNER_API_KEY")
+# print(type(titles)) #> <class 'bs4.element.ResultSet'> (like a list)
+# print(titles[5]) #> <span class="title">Macbeth</span>
+# print(titles[5].text) #> Macbeth
+
+# booklinks = soup.find_all("li", "booklink")
+
+# books = []
+# for list_item in booklinks:
+#     title = list_item.find("span", "title").text #> "Shakespeare's Sonnets"
+#     author = list_item.find("span", "subtitle").text #> "William Shakespeare"
+#     downloads = list_item.find("span", "extra").text #> '830 downloads'
+#     downloads_count = int(downloads.replace(" downloads", "")) #> 830
+#     book = {"title": title, "author": author, "downloads": downloads_count}
+#     print(book)
+#     books.append(book)
+
+# print(books[2]["title"]) #> Macbeth
