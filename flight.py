e55d87d7a74fa7872a8a3ab119360e9106b7b037
KML122089 <65563104+KML122089@users.noreply.github.com>
2020-06-30 17:34:23 -0400

    Final Commit

--- flight.py
+++ flight.py
@@ -3,8 +3,22 @@ from lxml import html
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+# def to_usd(my_price):
+#     return f"${my_price:,.2f}" #> $12,000.71
 
+print("Welcome to Flight Recs for the cheapest deals from your city. Enter the airport code you want to travel from and the dates.")
+
+#airport_codes = ["ATL", "LAX"] ORD, DFW, DEN, JFK, SFO, SEA, LAS, MCO, EWR, CLT, PHX, IAH, MIA, BOS, MSP, FLL, DTW, PHL, LGA, BWI, SLC, SAN, IAD, DCA, MDW, TPA, PDX]
+
+# try:
 origin_code = input("TYPE YOUR AIRPORT CODE HERE:") #origin code
+    # if origin_code not in airport_codes:
+    #     print("Hmmm, I don't think that's a valid code")
+    # else:
+    #     if origin_code.isdigit() and origin_code in airport_codes:
+    #         origin_codes.append(origin_code)
+    # break
+
 start_date = input("TYPE YOUR DATE HERE (FORMAT:YYYY-MM-DD):") #flight date
 end_date = input("TYPE YOUR END DATE HERE OR PRESS ENTER FOR ONE WAY (FORMAT:YYYY-MM-DD):") #end date
 
@@ -12,6 +26,7 @@ end_date = input("TYPE YOUR END DATE HERE OR PRESS ENTER FOR ONE WAY (FORMAT:YYY
 
 driver = webdriver.Chrome("/Users/kellylynch/Downloads/chromedriver")
 driver.get(f"https://skiplagged.com/flights/{origin_code}/{start_date}/{end_date}")
+#driver.get("https://skiplagged.com/flights/JFK/2020-07-23/2020-07-30")
 soup = BeautifulSoup(driver.page_source, "lxml")
 
 # alllist = soup.find("section", id="trip-list-skipsy-wrapper", class_="skipsy-container").find_all("li")
@@ -28,6 +43,7 @@ destination_list = alllist.find_all("h2")
 for destination in destination_list:
     destinations.append(destination.text)
 
+
 prices = []
 prices_formatted_2 =[]
 price_list = alllist.find_all("div", class_="skipsy-cost")
@@ -36,10 +52,16 @@ for price in price_list:
     prices_formatted = prices[-1][-1]
     prices_formatted_2.append(prices_formatted)
 
+# prices_formatted_4 = []
+# for price_2 in prices_formatted_2:
+#     prices_formatted_3 = to_usd(float(prices_formatted_2)) 
+#     prices_formatted_4.append(prices_formatted_3)
+  
 combined_d_p = list(zip(destinations, prices_formatted_2))[:20]
 for elements in combined_d_p:
-    print (elements)
+    print(elements)
 
+print("Bon Voyage!")
 driver.quit()
 
 # alllist = soup.find("section", id="trip-list-skipsy-wrapper", class_="skipsy-container")
